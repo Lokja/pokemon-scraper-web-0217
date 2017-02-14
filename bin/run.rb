8f52140273b0316223846b6c5006c79f39848be8
Devin Rieger <dev0089@gmail.com>
2017-02-14 01:29:12 -0500

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -5,3 +5,5 @@ Scraper.new(@db).scrape
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
 # test out your code here!
+lokja = Pokemon.find(25, @db)
+binding.pry
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,36 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db, :hp
+
+  @@all = []
+
+  def initialize(poke_hash)
+    @id = poke_hash[:id]
+    @name = poke_hash[:name]
+    @type = poke_hash[:type]
+    @db = poke_hash[:db]
+    @hp = nil
+    @hp ||= poke_hash[:hp]
+    @@all << self
+  end
+
+  def alter_hp(hp, db)
+    db.execute("UPDATE pokemon SET hp = (?) WHERE id = (?)", hp, id)
+  end
+
+  def self.all
+    @@ll
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
+  def self.find(id, db)
+    newPoke = db.execute("SELECT * FROM pokemon WHERE id = (?)", id).flatten
+    #binding.pry
+    newPokehash = {id: newPoke[0], name: newPoke[1], type: newPoke[2], hp: newPoke[3], db: db}
+    self.new(newPokehash)
+  end
+
 end
